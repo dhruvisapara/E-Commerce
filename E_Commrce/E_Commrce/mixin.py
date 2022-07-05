3c19fa19798c7e7c450c9737b2b22a6cdc68f6e6
dhruvisapara <dhruvi.sapara@trootech.com>
2022-07-05 09:25:08 +0530

    add gitignore file

--- E_Commrce/E_Commrce/mixin.py
+++ E_Commrce/E_Commrce/mixin.py
@@ -3,11 +3,10 @@ from rest_framework.renderers import JSONRenderer
 
 class CustomRenderer(JSONRenderer):
 
-    def render(self, data, accepted_media_type=None, renderer_context=None):
+    def render(self, data,accepted_media_type=None, renderer_context=None):
         response = {
+            'message':data.get("message"),
+            'data': data.get('result')
 
-            'message': 'Successfully',
-            'data': data
         }
-
         return super(CustomRenderer, self).render(response, accepted_media_type, renderer_context)
